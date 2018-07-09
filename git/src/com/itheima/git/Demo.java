e64d3ffabcf1ee74b6cd9db7df4c643d412678dd
hulei <13109513283.163.com>
2018-07-09 15:17:55 +0800

    第一次提交

---
+++ git/src/com/itheima/git/Demo.java
@@ -0,0 +1,31 @@
+/**   
+* @Title: Demo.java 
+* @Package com.itheima.git 
+* @Description: TODO(一句话简述该文件的作用) 
+* @author 胡磊   
+* @date 2018年7月9日 下午3:04:51 
+* @version V1.0   
+*/
+package com.itheima.git;
+
+/** 
+* @ClassName: Demo 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author 胡磊 
+* @date 2018年7月9日 下午3:04:51 
+*  
+*/
+public class Demo {
+
+	/** 
+	* @Title: main 
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param args    设定文件 
+	* @return void    返回类型 
+	* @throws 
+	*/
+	public static void main(String[] args) {
+		System.out.println("11111111111");
+	}
+
+}
